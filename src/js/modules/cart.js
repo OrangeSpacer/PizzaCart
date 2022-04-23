7ee84bd5b9fa15ab0948fccb1206a6ecab2b050d
OrangeSpacer <maxaev.art@yandex.ru>
2022-04-23 15:25:23 +0300

    feat: add trashcan script

--- src/js/modules/cart.js
+++ src/js/modules/cart.js
@@ -29,25 +29,27 @@ export const cart = (cartItemBlock,parentItem,buttonAddItem,titleItem,priceItem,
                     const cartItem = 
                 `
                 <div class="product__cart_item" data-id="${cardItem.id}">
-                <div class="product__cart_item-left">
-                    <img src="${cardItem.img}" class="product__cart_view" alt="prodcut3">
-                </div>
-                <div class="product__cart_item-right">
-                    <h2 class="product__cart_title">
-                        ${cardItem.title}
-                    </h2>
-                    <div class="product__cart_price">
-                        ${cardItem.price}
+                    <div class="product__cart_item-left">
+                        <img src="${cardItem.img}" class="product__cart_view" alt="prodcut3">
                     </div>
-                    <div class="product__cart_descr">
-                        ${cardItem.descr}
-                    </div>
-                    <div class="product__quantity">
-                        <button class="btn btn__minus"></button>
-                        <div class="product__quantity_number">
-                            ${cardItem.quantity}
+                    <div class="product__cart_item-right">
+                        <h2 class="product__cart_title">
+                            ${cardItem.title}
+                        </h2>
+                        <div class="product__cart_price">
+                            ${cardItem.price}
                         </div>
-                        <button class="btn btn__plus"></button>
+                        <div class="product__cart_descr">
+                            ${cardItem.descr}
+                        </div>
+                        <div class="product__quantity">
+                            <button class="btn btn__minus"></button>
+                            <div class="product__quantity_number">
+                                ${cardItem.quantity}
+                            </div>
+                            <button class="btn btn__plus"></button>
+                        </div>
+                        <div class="product__trashcan"></div>
                     </div>
                 </div>
                 `
---
+++ src/js/modules/trashcan.js
@@ -0,0 +1,14 @@
+export const  trashcan = (trashButton) => {
+    const cartBlock = document.querySelector('.product__cart_items')
+
+    cartBlock.addEventListener('click', (e) => {
+        let target = e.target
+
+        if(target.classList.contains(trashButton)){
+            let parentNumber = (target.closest('.product__cart_item').dataset.id)
+            let parent = cartBlock.querySelector(`.product__cart_item[data-id="${parentNumber}"]`)
+            parent.remove()
+        }
+    })
+    
+}
